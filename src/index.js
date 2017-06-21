abdd5b61e05bdb6c47a19d34c5f5b6b4bd6b08a5
Zhoodar <zhoodar.abdikadyr@zensoft.io>
2017-06-21 18:04:16 +0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ActivityLog from './ActivityLog';
 import registerServiceWorker from './service/registerServiceWorker';
 import './stylesheets/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ActivityLog />, document.getElementById('root'));
 registerServiceWorker();
